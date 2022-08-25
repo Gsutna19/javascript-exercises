c607aca630b9020e82af1ee5deb8d440bf42ca20
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-08-25 09:31:15 -0400

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = string;
+    if (num == 0) {
+        word = '';
+    } else if (num < 0) {
+        word = 'ERROR';
+    } else {
+        for(let i = 1; i < num; i++) {
+            word += string;
+        }
+    }
+    return word;
 };
 
 // Do not edit below this line
