5ddfdeae942add9116877b5d98de8e2ff2cd465c
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-08-25 10:04:46 -0400

    Complete exercise(brilliantly, if I may add)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let words = string;
+    const stringArray = words.split("");
+    let length = stringArray.length;
+    const reverseStringArray = [];
 
+    for (let i = 0; i <= length; i++) {
+
+        reverseStringArray.push(stringArray[length - i]);
+    }
+    return reverseStringArray.join('');
 };
 
 // Do not edit below this line
