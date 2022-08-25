bf1acb8ac9f7ccd261ee5354f422a0898ca57926
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-08-25 10:59:44 -0400

    Complete exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    
+    let sum = 0;
+    if (startNum < 0 || endNum < 0) {
+        sum = "ERROR";
+    } else if (typeof endNum === 'string' || typeof startNum === 'string') {
+        sum = "ERROR";
+    } else if (startNum <= endNum) {
 
+        for (let i = startNum; i <= endNum; i++) {
+            sum += i;
+        }
+    } else if (endNum < startNum) {
+        for (let i = endNum; i <= startNum; i++) {
+            sum += i;
+        }
+    } else if (isNaN(startNum) || isNaN(endNum)) {
+        sum = "ERROR";
+    }
+    return sum
 };
 
 // Do not edit below this line
