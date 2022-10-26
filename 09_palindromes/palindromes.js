289807925bb472a559eca733b4938d4784e8f83c
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-10-26 12:41:38 -0400

    Complete exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    const lowString = string.toLowerCase(); //Make everything lower case
+    let alpha = lowString.replace(/\W/g, "");   //Check for non-alphanumeric values
+
+    const symbols = alpha.split("");    //Create an array with each item
+
+    const reversed = symbols.reverse();
+
+    const joint = reversed.join("");
+
+    if (alpha == joint) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    const num = parseInt(x);
+    const series = [];
+
+    let prev = 0;
+    let current = 1;
+    let next;
+    for (let i = 0; i <= num; i++) {
+        series.push(prev);
+        next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    if (x < 0) {
+        return "OOPS";
+    }
+
+    return series[num];
 };
 
 // Do not edit below this line
