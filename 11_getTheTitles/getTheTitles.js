3ec067e361045595d35e2d90c24f7a56ca681640
Gsutna19 <fe.lipe19menezes@gmail.com>
2022-10-28 11:18:52 -0400

    Finished exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    const allBooks = books.map(book => `${book.title}`);
+
+    // const allBooks = [];
+
+    // for(let i = 0; i < books.length; i++) {
+    //     allBooks.push(books[i].title);
+    // }
+
+    return allBooks;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Get current year
+    const currentYear = (new Date()).getFullYear();
+
+    // If person is living, return current year
+
+
+    // Sort by oldest first
+    const age = people.sort(function(a, b) {
+
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = currentYear;
+        } else if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = currentYear;
+        }
+        // console.log(a.yearOfDeath);
+        // console.log(b.yearOfDeath);
+
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    //Help debugging
+    // console.log(age);
+
+    // const oldest = people.find(function(person) {
+    //     // if(!person.yearOfDeath) {}
+
+
+
+    // })
+
+    return age[0];
 
 };
 
